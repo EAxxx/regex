b9998b88bd5499c70f67de0b8c87c4ed6558f7cd
Tiago Tavares <tiagoft@gmail.com>
2017-06-22 09:40:14 -0300

    Coloquei codigo-base no repositorio

--- src/main.c
+++ src/main.c
@@ -1,17 +1,39 @@
-/* Contador de palavras
+/* Validador de logins
  *
- * Este programa recebera uma serie de caracteres representando palavras em sua
- * entrada. Ao receber um caractere fim de linha ('\n'), deve imprimir na tela o
- * numero de palavras separadas que recebeu e, apos, encerrar.
+ * Este programa realiza a validacao de logins e e-mails
  */
 
 #include <stdio.h>
+#include <regex.h>
 
-int main() {
+ int main() {
 
-  int x, y;
+  /* Variaveis que receberao as entradas do sistema*/
+  char login[50];
+  char email[50];
 
-  scanf("%d %d\n", &x, &y);
-  printf("%d\n", x + 200);
+  /* Armazena uma expressao regular */
+  regex_t regex_login;
+
+  /* Guarda mensagens de erro */
+  int ret;
+
+  /* Receber entradas */
+  scanf("%s %s\n", login, email);
+
+  /* Compila uma expressao regular */
+  ret = regcomp(&regex_login, "[a-z]*", 0);
+  if (ret != 0) {
+    printf("Erro: expressao regular nao compilada!\n");
+    return 1;
+  }
+
+  /* Executa avaliador de expressao regular e busca um match */
+  if (regexec(&regex_login, login, 0, NULL, 0) == 0) {
+    printf("ACEITAR\n");
+  } else {
+    printf("REJEITAR\n");
+  }
+
   return 0;
 }
